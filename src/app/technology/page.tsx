6f74dd8cf872a72e907f8dac1f5fdfc1ea1c450b
Abu Hanjala <abuhanjala656@gmail.com>
2026-07-27 19:54:36 +0530

    update technology page

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -3,6 +3,12 @@ import Navbar from "@/components/layout/Navbar/Navbar";
 import Footer from "@/components/layout/Footer/Footer";
 import TechnologyHero from "@/components/sections/Technology/TechnologyHero";
 import TechnologyGrid from "@/components/sections/Technology/TechnologyGrid";
+import CTA from "@/components/sections/Home/cta";
+import TechnologyStack from "@/components/sections/Services/TechnologyStack";
+import TechnologyFaq from "@/components/sections/Technology/Technologyfaq";
+import TechnologyOverview from "@/components/sections/Technology/TechnologyOverview";
+import Stats from "@/components/sections/Home/stats";
+import IndustryGrid from "@/components/sections/Services/IndustryGrid";
 
 export const metadata: Metadata = {
   title: "Technology | nodalXpoint",
@@ -14,7 +20,13 @@ export default function TechnologyPage() {
     <main className="bg-canvas">
       <Navbar />
       <TechnologyHero />
+      <TechnologyOverview/>
       <TechnologyGrid />
+      <TechnologyStack/>
+      <IndustryGrid />
+      <Stats/>
+      <TechnologyFaq/>
+      <CTA/>
       <Footer />
     </main>
   );
